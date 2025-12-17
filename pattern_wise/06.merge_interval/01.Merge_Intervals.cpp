0a1f94af6397b4862bb079190adcc0315eab413e
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2025-12-17 13:15:17 +0000

    day 40

---
+++ pattern_wise/06.merge_interval/01.Merge_Intervals.cpp
@@ -0,0 +1,28 @@
+//date-17/12/25
+//link-https://leetcode.com/problems/merge-intervals/
+/*Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].*/
+//code-
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    if (intervals.empty()) return {};
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> merged;
+    merged.push_back(intervals[0]);
+    for (int i = 1; i < intervals.size(); i++) 
+        if (intervals[i][0] <= merged.back()[1]) {
+            merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+        } else {
+            merged.push_back(intervals[i]);
+        }
+    }
+
+    return merged;
+    }
+};
