c38bfbe220fa7dc4c2d9595b247b73e154d6d3d6
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2025-12-31 13:06:53 +0000

    day 54

---
+++ pattern_wise/10.tree_bsf/01.BT_level_traverse.cpp
@@ -0,0 +1,58 @@
+//date-31/12/25
+//link-
+/*Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]*/
+
+//code-
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+      vector<vector<int>> result;
+        
+        if (root == NULL)
+            return result;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> level;
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                level.push_back(node->val);
+                
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+        
+            result.push_back(level); 
+        }
+           return result;
+        
+    }
+};
