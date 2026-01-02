d695b39b91e8a1cc098eacaa8f7706b010e29f38
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2026-01-02 13:15:53 +0000

    day 56

--- pattern_wise/10.tree_bsf/03.binary_tree_zigzag.cpp
+++ pattern_wise/10.tree_bsf/03.binary_tree_zigzag.cpp
@@ -1,19 +1,14 @@
-//date-01/1/26
-//link-https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
-/*Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
-
- 
+//adte-2/01/26
+//link-https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+/*Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
 
 Example 1:
 
-
 Input: root = [3,9,20,null,null,15,7]
-Output: [[15,7],[9,20],[3]]*/
-
+Output: [[3],[20,9],[15,7]]*/
 //code-
 #include <bits/stdc++.h>
 using namespace std;
-
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -22,27 +17,35 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
- 
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-       vector<vector<int>> result;
-        if (root == NULL) return result;
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
         queue<TreeNode*> q;
         q.push(root);
+        bool leftToRight = true;
+
         while (!q.empty()) {
             int size = q.size();
-            vector<int> level;
+            vector<int> level(size);
+
             for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
-                level.push_back(node->val);
+
+                int index = leftToRight ? i : size - 1 - i;
+                level[index] = node->val;
+
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
+
             result.push_back(level);
+            leftToRight = !leftToRight;
         }
-        reverse(result.begin(), result.end());
-        return result; 
+
+        return result;
     }
 };
