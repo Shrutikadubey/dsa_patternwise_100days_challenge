9fdc6a4c284a36d26d3eea00b30d8b020bc92569
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2026-01-12 12:00:08 +0000

    day 66

---
+++ pattern_wise/10.tree_bsf/12.search_2d_matrix_II.cpp
@@ -0,0 +1,32 @@
+//date-12/1/26
+//link-https://leetcode.com/problems/search-a-2d-matrix-ii/
+/*Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true*/
+
+//code-
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();  
+        int row = 0;
+        int col = n - 1;
+        
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        } 
+        return false;
+    }
+};
