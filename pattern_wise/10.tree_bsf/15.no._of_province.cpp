1345307fa0b31a1cbc48b32b2ca3040ae9103513
Shrutikadubey <113886718+Shrutikadubey@users.noreply.github.com>
2026-01-15 12:34:43 +0000

    day 69

---
+++ pattern_wise/10.tree_bsf/15.no._of_province.cpp
@@ -0,0 +1,37 @@
+//15/1/26
+//link-https://leetcode.com/problems/number-of-provinces/description/
+/*There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+Return the total number of provinces.
+
+Example 1:
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2*/
+//code-
+#include <vector>
+using namespace std;
+class Solution {
+public:
+void dfs(int city, vector<vector<int>>& isConnected, vector<bool>& visited) {
+        visited[city] = true;
+        
+        for (int neighbor = 0; neighbor < isConnected.size(); neighbor++) {
+            if (isConnected[city][neighbor] == 1 && !visited[neighbor]) {
+                dfs(neighbor, isConnected, visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int provinces = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+};
